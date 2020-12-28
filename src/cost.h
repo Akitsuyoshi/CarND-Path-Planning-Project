2823ca61fc258fc75792e1021262cf67a76c9361
Tsuyoshi Akiyama <ka090824@gmail.com>
2020-12-28 23:21:43 +0900

    feat(cost.h): add cost functions header file

---
+++ src/cost.h
@@ -0,0 +1,23 @@
+#ifndef COST_H
+#define COST_H
+
+// Weights for each cost functions
+const float COLLISION = pow(10, 6);
+const float BUFFER = pow(10, 5);
+
+float collision_cost() {
+  float cost = 0;
+  return cost;
+}
+
+float buffer_cost() {
+  float cost = 0;
+  return cost;
+}
+
+float calculate_cost() {
+  float cost = 0;
+  return cost;
+}
+
+#endif  // COST_H
