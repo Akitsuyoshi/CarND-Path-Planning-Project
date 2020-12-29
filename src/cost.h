42cf2e50643e200b15e0daab68243e756d175063
Tsuyoshi Akiyama <ka090824@gmail.com>
2020-12-29 14:36:12 +0900

    feat(cost.h): implement collision and calculate cost functions

--- src/cost.h
+++ src/cost.h
@@ -1,27 +1,34 @@
 #ifndef COST_H
 #define COST_H
 
+using std::vector;
+
+const int BUFFER_BETWEEN_CAR = 30;
 // Weights for each cost functions
 const float COLLISION = pow(10, 6);
 const float BUFFER = pow(10, 5);
 
-float collision_cost() {
-  float cost = 0.;
-  return cost;
+float logistic(double x) { return 2.0 / (1 + exp(-x)) - 1.0; }
+
+float collision_cost(double car_s, double check_car_s) {
+  if (abs(check_car_s - car_s) < BUFFER_BETWEEN_CAR) {
+    return 1.;
+  }
+  return 0.;
 }
 
-float buffer_cost() {
-  float cost = 0.;
-  return cost;
+float buffer_cost(double car_s, double check_car_s) {
+  return logistic(BUFFER_BETWEEN_CAR / abs(check_car_s - car_s));
 }
 
-float calculate_cost() {
-  vector<std::function> cost_functions = {collision_cost, buffer_cost};
+float calculate_cost(double car_s, double check_car_s) {
+  vector<std::function<float(double car_s, double check_car_s)>>
+      cost_functions = {collision_cost, buffer_cost};
   vector<float> weights = {COLLISION, BUFFER};
   float cost = 0.;
 
   for (int i = 0; i < cost_functions.size(); i++) {
-    cost += weights[i] * cost_functions[i]();
+    cost += weights[i] * cost_functions[i](car_s, check_car_s);
   }
   return cost;
 }
